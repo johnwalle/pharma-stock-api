59f4d97a56c7637ec87928a801f39a328a317d8b
mark133227 <etawe1627@gmail.com>
2025-07-06 08:33:09 -1200

    Add login,forgot and reset password endpoints

---
+++ routes/auth.routes.ts
@@ -0,0 +1,17 @@
+import express from 'express';
+import { authLimiter } from '../middlewares/auth.limmiter';
+import {
+  login,
+  refreshToken,
+  forgotPassword,
+  resetPassword,
+} from '../controllers/auth.controller';
+
+const router = express.Router();
+
+router.post('/login', authLimiter, login);
+router.post('/refresh-token', refreshToken);
+router.post('/forgot-password', forgotPassword);
+router.post('/reset-password/:token', resetPassword);
+
+export default router;
