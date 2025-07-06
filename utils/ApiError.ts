a145bce15179aa90c0e9d577211643505cd9e96d
mark133227 <etawe1627@gmail.com>
2025-07-06 08:39:01 -1200

    Add custom ApiError calss for standardize error handling

---
+++ utils/ApiError.ts
@@ -0,0 +1,36 @@
+class ApiError extends Error {
+  statusCode: number;
+  isOperational: boolean;
+
+  constructor(
+    statusCode: number,
+    message: string,
+    isOperational = true,
+    stack = ''
+  ) {
+    super(message);
+
+    // Set the prototype explicitly (important for extending Error in TS)
+    Object.setPrototypeOf(this, new.target.prototype);
+
+    this.statusCode = statusCode;
+    this.isOperational = isOperational;
+
+    if (stack) {
+      this.stack = stack;
+    } else {
+      Error.captureStackTrace(this, this.constructor);
+    }
+  }
+
+  toJSON() {
+    return {
+      error: true,
+      code: this.statusCode,
+      message: this.message,
+      stack: this.stack,
+    };
+  }
+}
+
+export default ApiError;
