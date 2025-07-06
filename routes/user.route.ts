c7631a0d4820003ef47face3e281b28edc3df48e
mark133227 <etawe1627@gmail.com>
2025-07-06 08:33:33 -1200

    Implment user profile CRUD routes

---
+++ routes/user.route.ts
@@ -0,0 +1,16 @@
+import { Router } from 'express';
+import { register, logout, deleteUser, updateUser, createUserByAdmin } from '../controllers/user.controller';
+import { requireSignIn, adminMiddleware } from '../middlewares/auth.middleware';
+
+
+const router = Router();
+
+
+router.post('/register', register);
+router.post('/logout', logout);
+router.post('/create-user', requireSignIn, adminMiddleware, createUserByAdmin);
+router.put('/update/:id', requireSignIn, updateUser);
+router.delete('/delete-user/:id', requireSignIn, adminMiddleware, deleteUser); // ✅ protected delete
+
+
+export default router;
