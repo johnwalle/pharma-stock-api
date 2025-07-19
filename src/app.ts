4b24059ad7c67adaa4e468754ec2a78ea25f4dab
mark133227 <etawe1627@gmail.com>
2025-07-18 18:40:32 -1200

    implement audit logging

--- src/app.ts
+++ src/app.ts
@@ -7,6 +7,8 @@ import userRoutes from '../routes/user.route'
 import medicineRoutes from '../routes/medicine.routes';
 import sellRoutes from '../routes/sell.routes';
 import reportRoutes from '../routes/report.routes';
+import auditLogRoutes from '../routes/auditLog.route';
+
 const app = express();
 
 // Global middlewares
@@ -21,6 +23,7 @@ app.use('/api/users', userRoutes);
 app.use('/api/medicines', medicineRoutes);
 app.use('/api/sell', sellRoutes);
 app.use('/api/reports', reportRoutes);
+app.use('/api/audit-logs', auditLogRoutes);
 
 // Optional: Root health check
 app.get('/', (_, res) => {
