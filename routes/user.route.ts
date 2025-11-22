852bd06a6e4353874559d132d4073d1bdf56bf9a
Yared3214 <yaredman0099@gmail.com>
2025-11-21 20:12:24 -0800

    updated user route to add route to save fcm token

--- routes/user.route.ts
+++ routes/user.route.ts
@@ -1,5 +1,5 @@
 import { Router } from 'express';
-import { register, logout, getUsers, deleteUser, updateUser, createUserByAdmin } from '../controllers/user.controller';
+import { register, logout, getUsers, deleteUser, updateUser, createUserByAdmin, saveFcmToken } from '../controllers/user.controller';
 import { requireSignIn, adminMiddleware } from '../middlewares/auth.middleware';
 
 
@@ -10,6 +10,7 @@ router.post('/register', register);
 router.post('/logout', logout);
 router.post('/create-user', requireSignIn, adminMiddleware, createUserByAdmin);
 router.put('/update/:id', requireSignIn, updateUser);
+router.put('/save-fcmToken', requireSignIn, saveFcmToken);
 router.delete('/delete-user/:id', requireSignIn, adminMiddleware, deleteUser); // ✅ protected delete
 
 // get all users
